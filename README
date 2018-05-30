ee3706d09fd5cfce563b8af8db5140569f8ef88d
Merca Ionel <ionel.merca@gmail.com>
2018-05-30 14:10:38 +0300

    Spacy doc - with spacy build from source with Romanian

---
+++ README
@@ -0,0 +1,12 @@
+Build and install spacy locally
+
+1. Navigate to spacy/
+2. pip3 install -r requirements.txt
+3. python3 setup.py build_ext --inplace (for error regarding cython use sudo - it worked for me)
+4. Install from source: sudo python3 setup.py install 
+5. Check version of spacy: pip3 show spacy (should have a dev version)
+
+
+Install spacy named entity recognition multi language
+python -m spacy download xx_ent_wiki_sm
+
---
+++ spacy/spacy/lang/ro/lex_attrs.py
@@ -0,0 +1,40 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+from ...attrs import LIKE_NUM
+
+_num_words = set("""
+zero unu una doi doua trei patru cinci șase șapte opt nouă zece
+unsprezece doisprezece treisprezece paisprezece cincisprezece șaisprezece șaptesprezece
+optsprezece nouăsprezece douazeci treizeci patruzeci cincizeci șaizeci șaptezeci
+optzeci nouăzeci sută mie milion miliard 
+    """)
+
+_ordinal_words = set("""
+prima primul doua doilea treia treilea patra patrulea cincea cincilea 
+șasea șaselea șaptelea optulea noua nouălea zecelea zecea unsprezecelea doisprezecelea
+treisprezecelea paisprezecelea cincisprezecelea șaisprezecelea șaptesprezecea optsprezecea optsprezecelea
+douăzeci treizeci cincizecilea șaizecilea șaptezecilea
+""".split())
+
+
+def like_num(text):
+    # Might require more work?
+    # See this discussion: https://github.com/explosion/spaCy/pull/1161
+    text = text.replace(',', '').replace('.', '')
+    if text.isdigit():
+        return True
+    if text.count('/') == 1:
+        num, denom = text.split('/')
+        if num.isdigit() and denom.isdigit():
+            return True
+    if text.lower() in _num_words:
+        return True
+    if text.lower() in _ordinal_words:
+        return True
+    return False
+
+
+LEX_ATTRS = {
+    LIKE_NUM: like_num
+}
