4f63d3d7c0948dd3b3753a07171ff54feb484b58
Stefan Ruseti <stefan.ruseti@cs.pub.ro>
2018-10-17 16:54:30 +0300

    added isalive endpoint

--- readerbench/server.py
+++ readerbench/server.py
@@ -26,6 +26,13 @@ def create_spacy_doc():
 
     return response, 200
 
+@app.route('/isalive', methods=['GET'])
+def check_connection():
+    doc = spacyInstance.process(request.json['doc'])
+    response = jsonify("alive")
+    return response, 200
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8082, debug=False)
